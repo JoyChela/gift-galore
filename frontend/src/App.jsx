39822c267981ba129472703a53bb07803d3224ae
mark-s-bit <markchege03@gmail.com>
2024-10-17 14:54:16 +0300

    done eeror page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
-// App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginRegister from "./components/LoginRegister"; // Adjust path as necessary
 import Payment from "./components/Payment"; // Import Payment component
+import ErrorPage from "./components/ErrorPage"; // Import ErrorPage component
 
 function App() {
   return (
@@ -10,6 +10,9 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginRegister />} /> {/* Home/Login Page */}
         <Route path="/payment" element={<Payment />} /> {/* Payment Page */}
+        
+        {/* Catch-all route for undefined paths */}
+        <Route path="*" element={<ErrorPage />} /> {/* Error page */}
       </Routes>
     </Router>
   );
