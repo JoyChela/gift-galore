77706efdc9967c574376ba13c7e708feb1f35874
mark-s-bit <markchege03@gmail.com>
2024-10-17 09:21:27 +0300

    done payment &&  Loginpage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// index.jsx or main.jsx
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"; // Link to App.jsx
+import "./index.css"; // Optional: Link to any global styles
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+  document.getElementById("root") // Make sure this matches your index.html
+);
